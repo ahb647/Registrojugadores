77b9a2e9392f6d3bd7918c1c07ac75d470070c85
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-09 17:41:07 -0400

    Se creó el estado de datos para los jugadores

---
+++ app/src/main/java/edu/ucne/registrojugadores/ui/theme/screen/Jugadores/JugadoresState.kt
@@ -0,0 +1,7 @@
+package edu.ucne.registrojugadores.ui.theme.screen.Jugadores
+
+data class JugadoresState(
+    val jugadorId: Int? = null,
+    val nombres: String = "",
+    val partidas: Int = 0
+)
