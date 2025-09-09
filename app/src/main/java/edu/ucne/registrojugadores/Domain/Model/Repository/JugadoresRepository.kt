7d83db2cb34b908f796d4740da53d04a8ef5a291
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-09 17:39:20 -0400

    Se creo y configuro el repositorio para jugadores

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/Model/Repository/JugadoresRepository.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registrojugadores.Domain.Model.Repository
+
+import edu.ucne.registrojugadores.Domain.Model.Model.Jugadores
+import kotlinx.coroutines.flow.Flow
+
+interface JugadoresRepository {
+
+    fun getAllJugadores(): Flow<List<Jugadores>>
+
+    suspend fun getJugadorById(id: Int): Jugadores?
+
+    suspend fun insertJugador(jugador: Jugadores)
+
+    suspend fun deleteJugador(jugador: Jugadores)
+
+    suspend fun updateJugador(jugador: Jugadores)
+}
