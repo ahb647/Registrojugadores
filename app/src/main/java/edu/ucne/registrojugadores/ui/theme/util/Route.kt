9dfa7f5434803bad62a51e6c15aa7b2c0ba995bc
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-14 18:32:33 -0400

    Se añadieron constantes para las rutas de la lista de partidas y el formulario de partidas.

--- app/src/main/java/edu/ucne/registrojugadores/ui/theme/util/Route.kt
+++ app/src/main/java/edu/ucne/registrojugadores/ui/theme/util/Route.kt
@@ -4,4 +4,8 @@ object Route {
     const val JUGADOR_LIST = "jugador_list"
     const val JUGADOR_FORM = "jugador_form"
 
+    const val PARTIDA_LIST = "partida_list"
+
+    const val PARTIDA_FORM = "partida_form"
+
 }
