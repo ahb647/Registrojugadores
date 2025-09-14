d945cad67e8a720f756381bf7c0c404aa7e55884
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-14 18:30:13 -0400

    Se agregó la clase Partida como modelo de datos, incluyendo propiedades y un método para obtener la fecha actual.

---
+++ app/src/main/java/edu/ucne/registrojugadores/Domain/Model/Model/Partidas.kt
@@ -0,0 +1,25 @@
+package edu.ucne.registrojugadores.Domain.Model.Model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.*
+
+@Parcelize
+data class Partida(
+    val partidaId: Int = 0,
+    val fecha: String = "",
+    val jugador1Id: Int = 0,
+    val jugador2Id: Int = 0,
+    val ganadorId: Int = 0,
+    val esFinalizada: Boolean
+) : Parcelable {
+    companion object {
+        fun getFechaActual(): String {
+            val calendar = Calendar.getInstance()
+            val dia = calendar.get(Calendar.DAY_OF_MONTH)
+            val mes = calendar.get(Calendar.MONTH) + 1
+            val anio = calendar.get(Calendar.YEAR)
+            return "$dia/$mes/$anio"
+        }
+    }
+}
