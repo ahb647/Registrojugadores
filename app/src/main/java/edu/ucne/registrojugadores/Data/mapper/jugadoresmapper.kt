8e175bf9f3c56009bb716cdbad568947b36efb3f
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-14 18:29:34 -0400

    Se agregó el mapeo de la entidad Partida a su modelo correspondiente y se implementaron funciones para convertir entre Dominio y Entity.

--- app/src/main/java/edu/ucne/registrojugadores/Data/mapper/jugadoresmapper.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/mapper/jugadoresmapper.kt
@@ -1,9 +1,12 @@
 package edu.ucne.registrojugadores.Data.mapper
 
 import edu.ucne.registrojugadores.Data.Local.Entity.JugadorEntity
+import edu.ucne.registrojugadores.Data.Local.Entity.PartidaEntity
 import edu.ucne.registrojugadores.Domain.Model.Model.Jugadores
+import edu.ucne.registrojugadores.Domain.Model.Model.Partida
 
-// Convertir Entity -> Dominio
+
+// Entity -> Dominio
 fun JugadorEntity.asExternalModel(): Jugadores {
     return Jugadores(
         jugadorId = jugadorId,
@@ -12,11 +15,37 @@ fun JugadorEntity.asExternalModel(): Jugadores {
     )
 }
 
-// Convertir Dominio -> Entity
+// Dominio -> Entity
 fun Jugadores.toEntity(): JugadorEntity {
     return JugadorEntity(
         jugadorId = jugadorId ?: 0,
         nombres = nombres,
         partidas = partidas
+    )
+}
+
+
+
+// Entity -> Dominio
+fun PartidaEntity.asExternalModel(): Partida {
+    return Partida(
+        partidaId = partidaId,
+        fecha = fecha,
+        jugador1Id = jugador1Id,
+        jugador2Id = jugador2Id,
+        ganadorId = ganadorId,
+        esFinalizada = esFinalizada
+    )
+}
+
+// Dominio -> Entity
+fun Partida.toEntity(): PartidaEntity {
+    return PartidaEntity(
+        partidaId = partidaId,
+        fecha = fecha,
+        jugador1Id = jugador1Id,
+        jugador2Id = jugador2Id,
+        ganadorId = ganadorId,
+        esFinalizada = esFinalizada
     )
 }
