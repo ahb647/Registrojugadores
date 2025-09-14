557d9a9a88aacc58c674e0f7c9d697debb18820f
ahb647 <58379725+ahb647@users.noreply.github.com>
2025-09-14 18:29:11 -0400

    Se actualizó la base de datos para incluir la entidad Partida y su DAO correspondiente

--- app/src/main/java/edu/ucne/registrojugadores/Data/Local/Jugadores.kt
+++ app/src/main/java/edu/ucne/registrojugadores/Data/Local/Jugadores.kt
@@ -3,13 +3,18 @@ package edu.ucne.registrojugadores.Data.Local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registrojugadores.Data.Local.Dao.JugadorDao
+import edu.ucne.registrojugadores.Data.Local.Dao.PartidaDao
 import edu.ucne.registrojugadores.Data.Local.Entity.JugadorEntity
+import edu.ucne.registrojugadores.Data.Local.Entity.PartidaEntity
 
 @Database(
-    entities = [JugadorEntity::class],
+    entities = [JugadorEntity::class, PartidaEntity::class],
     version = 1,
     exportSchema = false
 )
-abstract class Jugadores : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
+
+    // DAOs
     abstract fun jugadorDao(): JugadorDao
+    abstract fun partidaDao(): PartidaDao
 }
